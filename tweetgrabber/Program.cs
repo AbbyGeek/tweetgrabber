651bd4f88f4381a7611354350e49b4b3524263c7
West, Abby <abbyrosewest@gmail.com>
2019-04-12 11:43:16 -0400

    Hid keys and secrets

--- tweetgrabber/Program.cs
+++ tweetgrabber/Program.cs
@@ -7,35 +7,29 @@ namespace tweetgrabber
 {
     class Program
     {
-        //private static string _customerKey = "ePYV2uTt9Qbwznz2UMtUbgJf1";
-        //private static string _customerSecret = "LFQNqjtCkvI3fctV2SnBnlwj3PhAspsyg7Cgeq3Fx9lVnTai7U";
-        //private static string _accessToken = "2602556882-2rmLXmubMt4iGVLxHxq0CIzJIMOiVfFvdBrjUYg";
-        //private static string _accessSecret = "EdIQGksrunteio5umGv1FxcmlQmxK5J2Fk64ItrtIeqpe";
-
-        //private static TwitterService service = new TwitterService(_customerKey, _customerSecret, _accessToken, _accessSecret);
         static void Main()
         {
-
-        string _customerKey = "ePYV2uTt9Qbwznz2UMtUbgJf1";
-        string _customerSecret = "LFQNqjtCkvI3fctV2SnBnlwj3PhAspsyg7Cgeq3Fx9lVnTai7U";
-        string _accessToken = "2602556882-2rmLXmubMt4iGVLxHxq0CIzJIMOiVfFvdBrjUYg";
-        string _accessSecret = "EdIQGksrunteio5umGv1FxcmlQmxK5J2Fk64ItrtIeqpe";
-
-
-        TwitterService service = new TwitterService(_customerKey, _customerSecret, _accessToken, _accessSecret);
+            string[] lines = File.ReadAllLines("../../../settings.txt");
+            string _customerKey = lines[0];
+            string _customerSecret = lines[1];
+            string _accessToken = lines[2];
+            string _accessSecret = lines[3];
 
 
-        var options = new ListTweetsOnUserTimelineOptions() { UserId = 3183325776, Count = 100, MaxId = null }; //Max ID is the ID of the furthest back tweet
+            TwitterService service = new TwitterService(_customerKey, _customerSecret, _accessToken, _accessSecret);
+
+
+        var options = new ListTweetsOnUserTimelineOptions() { UserId = 624953558, Count = 100, MaxId = null }; //Max ID is the ID of the furthest back tweet
             StringBuilder sb = new StringBuilder();
             int i = 0;
             int x = 0;
-            while (i < 100)
+            while (i < 50)
             {
                 var tweets = service.ListTweetsOnUserTimeline(options);
                 
                 foreach (var tweet in tweets)
                 {
-                    if (tweet.IsTruncated == false && !tweet.Text.Contains('@'))
+                    if (tweet.IsTruncated == false && !tweet.Text.Contains('@') && !tweet.Text.Contains("http"))
                     {
                         string text = tweet.Text.Replace("\n", " ");
                         sb.Append(text + "\n");
